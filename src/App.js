bfc4dca8437702371dd7d6718c4e31cb33362bc8
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-05-21 12:15:57 +0100

    project completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import axios from 'axios'
+const url='https://api.quotable.io/random'
 class App extends Component {
   state={
     quote:''
   }
+  componentDidMount(){
+    this.fetchAdvice()
+
+  }
+  fetchAdvice=()=>{
+    axios.get(url)
+    .then(res=>{
+  this.setState({quote:res.data.content})
+ 
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
   render() {
+ 
+    console.log(this.state)
     return (
-      <div>
-        
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.quote}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
       </div>
     );
   }
